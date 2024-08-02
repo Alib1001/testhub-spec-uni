bd2f88fd1614cd27175602613b86b1efcaa0489f
Alib1001 <superalibek123@gmail.com>
2024-08-02 16:00:02 +0500

    Post and getById methods in speciality rewrited

--- models/speciality.go
+++ models/speciality.go
@@ -8,20 +8,68 @@ import (
 )
 
 type Speciality struct {
-	Id            int           `orm:"auto" json:"id"`
-	Name          string        `orm:"size(128)" json:"name"`
-	Code          string        `orm:"size(64)" json:"code"`
-	VideoLink     string        `orm:"size(256)" json:"video_link"`
-	Description   string        `orm:"type(text)" json:"description"`
-	Universities  []*University `orm:"reverse(many)" json:"universities,omitempty"`
-	SubjectPair   *SubjectPair  `orm:"rel(fk);on_delete(set_null);null" json:"subject_pair,omitempty"`
-	CreatedAt     time.Time     `orm:"auto_now_add;type(datetime)" json:"created_at"`
-	UpdatedAt     time.Time     `orm:"auto_now;type(datetime)" json:"updated_at"`
-	PointStats    []*PointStat  `orm:"reverse(many)" json:"point_stats,omitempty"`
-	NameRu        string        `orm:"size(128)" json:"NameRu"`
-	NameKz        string        `orm:"size(128)" json:"NameKz"`
-	DescriptionRu string        `orm:"type(text)" json:"DescriptionRu"`
-	DescriptionKz string        `orm:"type(text)" json:"DescriptionKz"`
+	Id             int    `orm:"auto" json:"id"`
+	Name           string `orm:"size(128)" json:"name"`
+	NameRu         string `orm:"size(128)" json:"NameRu"`
+	NameKz         string `orm:"size(128)" json:"NameKz"`
+	AbbreviationRu string `json:"AbbreviationRu" validate:"required"`
+	AbbreviationKz string `json:"AbbreviationKz" validate:"required"`
+	Code           string `orm:"size(64)" json:"code"`
+	VideoLink      string `orm:"size(256)" json:"video_link"`
+	Description    string `orm:"type(text)" json:"description"`
+	DescriptionRu  string `orm:"type(text)" json:"DescriptionRu"`
+	DescriptionKz  string `orm:"type(text)" json:"DescriptionKz"`
+	AvgSalary      int    `json:"avg_salary"`
+	Degree         string `orm:"size(128)" json:"degree"`
+	Term           int
+	Scholarship    bool
+	Universities   []*University `orm:"reverse(many)" json:"universities,omitempty"`
+	SubjectPair    *SubjectPair  `orm:"rel(fk);on_delete(set_null);null" json:"subject_pair,omitempty"`
+	CreatedAt      time.Time     `orm:"auto_now_add;type(datetime)" json:"created_at"`
+	UpdatedAt      time.Time     `orm:"auto_now;type(datetime)" json:"updated_at"`
+	PointStats     []*PointStat  `orm:"reverse(many)" json:"point_stats,omitempty"`
+}
+
+type GetSpecialityResponse struct {
+	Id          int          `orm:"auto" json:"id"`
+	Name        string       `orm:"size(128)" json:"name"`
+	Degree      string       `orm:"size(128)" json:"degree"`
+	SubjectPair *SubjectPair `orm:"rel(fk);on_delete(set_null);null" json:"subject_pair,omitempty"`
+	PointStats  []*PointStat `orm:"reverse(many)" json:"point_stats,omitempty"`
+	AvgSalary   int          `json:"avg_salary"`
+	Code        string       `orm:"size(64)" json:"code"`
+	Scholarship bool
+}
+
+type AddSpecialityResponse struct {
+	Id             int    `form:"Id"`
+	NameRu         string `form:"NameRu" validate:"required"`
+	NameKz         string `form:"NameKz" validate:"required"`
+	AbbreviationRu string `form:"AbbreviationRu" validate:"required"`
+	AbbreviationKz string `form:"AbbreviationKz" validate:"required"`
+	SubjectPairID1 int    `form:"SubjectPairID1" validate:"required"`
+	SubjectPairID2 int    `form:"SubjectPairID2" validate:"required"`
+	Degree         string `form:"Degree" validate:"required"`
+	Code           string `form:"Code" validate:"required"`
+	Term           int    `form:"Term" validate:"required"`
+	Scholarship    bool   `form:"Scholarship"`
+	AvgSalary      int    `form:"AvgSalary" validate:"required"`
+}
+
+type GetByUniResponse struct {
+	SpecialityID    int      `orm:"column(speciality_id)" json:"speciality_id"`
+	SpecialityName  string   `orm:"column(speciality_name)" json:"speciality_name"`
+	UniversityName  string   `orm:"column(university_name)" json:"university_name"`
+	EducationFormat string   `orm:"column(education_format)" json:"education_format"`
+	Degree          string   `orm:"column(degree)" json:"degree"`
+	Scholarship     bool     `orm:"column(scholarship)" json:"scholarship"`
+	AvgSalary       int      `orm:"column(avg_salary)" json:"avg_salary"`
+	Term            int      `orm:"column(term)" json:"term"`
+	MinScore        int      `orm:"column(min_score)" json:"min_score"`
+	AnnualGrants    int      `orm:"column(annual_grants)" json:"annual_grants"`
+	Subject1ID      int      `orm:"column(subject1_id)" json:"subject1_id"`
+	Subject2ID      int      `orm:"column(subject2_id)" json:"subject2_id"`
+	SubjectNames    []string `json:"subject_names"`
 }
 
 type SpecialitySearchResult struct {
@@ -35,17 +83,47 @@ func init() {
 	orm.RegisterModel(new(Speciality))
 }
 
-func AddSpeciality(speciality *Speciality) (int64, error) {
+func AddSpecialityFromFormData(data *AddSpecialityResponse) (int64, error) {
 	o := orm.NewOrm()
-	id, err := o.Insert(speciality)
 
+	subject1 := Subject{Id: data.SubjectPairID1}
+	subject2 := Subject{Id: data.SubjectPairID2}
+
+	if err := o.Read(&subject1); err != nil {
+		return 0, fmt.Errorf("subject 1 not found: %v", err)
+	}
+	if err := o.Read(&subject2); err != nil {
+		return 0, fmt.Errorf("subject 2 not found: %v", err)
+	}
+
+	subjectPair := SubjectPair{
+		Subject1: &subject1,
+		Subject2: &subject2,
+	}
+
+	if _, err := o.Insert(&subjectPair); err != nil {
+		return 0, err
+	}
+
+	speciality := Speciality{
+		NameRu:         data.NameRu,
+		NameKz:         data.NameKz,
+		AbbreviationRu: data.AbbreviationRu,
+		AbbreviationKz: data.AbbreviationKz,
+		Degree:         data.Degree,
+		Code:           data.Code,
+		Term:           data.Term,
+		Scholarship:    data.Scholarship,
+		AvgSalary:      data.AvgSalary,
+		SubjectPair:    &subjectPair,
+	}
+
+	id, err := o.Insert(&speciality)
 	if err != nil {
 		return 0, err
 	}
 
-	speciality.Id = int(id)
-
-	return id, err
+	return id, nil
 }
 
 func GetSpecialityById(id int, language string) (*Speciality, error) {
@@ -162,30 +240,70 @@ func SearchSpecialities(params map[string]interface{}, language string) (*Specia
 	return result, nil
 }
 
-func GetSpecialitiesInUniversity(universityId int, language string) ([]*Speciality, error) {
+func GetSpecialitiesInUniversity(universityId int, language string) ([]GetByUniResponse, error) {
 	o := orm.NewOrm()
-	var specialities []*Speciality
-	num, err := o.QueryTable("speciality").
-		Filter("Universities__University__Id", universityId).
-		All(&specialities)
+	var results []GetByUniResponse
+
+	query := `
+		SELECT 
+			s.id as speciality_id,
+			CASE WHEN ? = 'ru' THEN s.name_ru ELSE s.name_kz END as speciality_name,
+			CASE WHEN ? = 'ru' THEN u.name_ru ELSE u.name_kz END as university_name,
+			CASE WHEN ? = 'ru' THEN u.study_format_ru ELSE u.study_format_kz END as education_format,
+			s.degree,
+			s.scholarship,
+			s.avg_salary,
+			s.term,
+			ps.min_score,
+			ps.annual_grants,
+			sp.subject1_id,
+			sp.subject2_id
+		FROM 
+			speciality s
+			INNER JOIN speciality_university su ON s.id = su.speciality_id
+			INNER JOIN university u ON su.university_id = u.id
+			LEFT JOIN point_stat ps ON s.id = ps.speciality_id AND u.id = ps.university_id
+			LEFT JOIN subject_pair sp ON s.subject_pair_id = sp.id
+		WHERE 
+			u.id = ?
+	`
+
+	_, err := o.Raw(query, language, language, language, universityId).QueryRows(&results)
 	if err != nil {
 		return nil, err
 	}
-	fmt.Printf("Number of specialities found: %d\n", num)
-	for _, speciality := range specialities {
-		fmt.Printf("Speciality: %+v\n", speciality)
 
-		// Переназначение полей в зависимости от языка
-		switch language {
-		case "ru":
-			speciality.Name = speciality.NameRu
-			speciality.Description = speciality.DescriptionRu
-		case "kz":
-			speciality.Name = speciality.NameKz
-			speciality.Description = speciality.DescriptionKz
+	for i := range results {
+		var subjectNames []string
+
+		if results[i].Subject1ID != 0 {
+			var subject1 Subject
+			err := o.QueryTable("subject").Filter("id", results[i].Subject1ID).One(&subject1)
+			if err == nil {
+				if language == "ru" {
+					subjectNames = append(subjectNames, subject1.NameRu)
+				} else {
+					subjectNames = append(subjectNames, subject1.NameKz)
+				}
+			}
 		}
+
+		if results[i].Subject2ID != 0 {
+			var subject2 Subject
+			err := o.QueryTable("subject").Filter("id", results[i].Subject2ID).One(&subject2)
+			if err == nil {
+				if language == "ru" {
+					subjectNames = append(subjectNames, subject2.NameRu)
+				} else {
+					subjectNames = append(subjectNames, subject2.NameKz)
+				}
+			}
+		}
+
+		results[i].SubjectNames = subjectNames
 	}
-	return specialities, err
+
+	return results, nil
 }
 
 func AssociateSpecialityWithSubjectPair(specialityId int, subjectPairId int) error {
@@ -280,7 +398,41 @@ func filterSpecialitiesInUniversity(params map[string]interface{}, specialities 
 		return specialities, nil
 	}
 
-	return GetSpecialitiesInUniversity(universityId, "")
+	// Получаем язык из параметров, если он существует, иначе используем язык по умолчанию
+	language, ok := params["language"].(string)
+	if !ok {
+		language = "ru" // язык по умолчанию
+	}
+
+	// Получаем специальности из университета
+	specialityResponses, err := GetSpecialitiesInUniversity(universityId, language)
+	if err != nil {
+		return nil, err
+	}
+
+	// Преобразуем specialityResponses в []*Speciality
+	var filteredSpecialities []*Speciality
+	for _, sr := range specialityResponses {
+		speciality := &Speciality{
+			Id:          sr.SpecialityID,
+			Name:        sr.SpecialityName,
+			Degree:      sr.Degree,
+			Scholarship: sr.Scholarship,
+			AvgSalary:   sr.AvgSalary,
+			Term:        sr.Term,
+		}
+
+		// Динамическое обновление имени в зависимости от языка
+		if language == "ru" {
+			speciality.NameRu = sr.SpecialityName
+		} else if language == "kz" {
+			speciality.NameKz = sr.SpecialityName
+		}
+
+		filteredSpecialities = append(filteredSpecialities, speciality)
+	}
+
+	return filteredSpecialities, nil
 }
 
 func filterSpecialitiesByName(params map[string]interface{}, specialities []*Speciality, language string) ([]*Speciality, error) {
